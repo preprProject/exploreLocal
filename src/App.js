2fd7d1215c9512b31a195a7443f0fdf261a3b660
Mila <mashtari1991@gmail.com>
2020-08-14 09:56:03 -0400

    form elements added

--- src/App.js
+++ src/App.js
@@ -20,12 +20,18 @@ class App extends Component {
         <h1>Find local bussiness around you</h1>
         <h2>support local and small bussiness recover from pandemic economic impact by reciving service at a local store</h2>
         <form>
-          <label></label>
-          <input></input>
-          <label></label>
-          <input></input>
-          <label></label>
-          <input></input>
+          <label htmlFor="searchInput" className="sr-only" ></label>
+          <input type="text" id="searchInput" placeholder="what are you looking for" value={this.state.searchTerm}></input>
+          <label htmlFor="location" className="sr-only"></label>
+          <input type="text" id="location" placeholder="location" value={this.state.location}></input>
+          <label ></label>
+          <select>
+            <option value="shops">shops</option>
+            <option value="hotels">hotels</option>
+            <option value="restuarant">restuarant</option>
+            <option value="events">events</option>
+          </select>
+          <button type="submit"></button>
         </form>
       </header>
       </Fragment>
